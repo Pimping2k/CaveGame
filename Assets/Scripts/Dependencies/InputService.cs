2e2c0751d27eb5737df79611c604db6c9079a6f0
Pimping . <auagfuiawg@mail.ru>
2025-06-14 19:12:11 +0300

    Added base service locator

---
+++ Assets/Scripts/Dependencies/InputService.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Dependencies
+{
+    public class InputService : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerComponents/Movement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace PlayerComponents
+{
+    public class Movement : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private float _speed;
+
+        #region Movement calculations
+
+        private void ApplyMovement()
+        {
+            
+        }
+
+        #endregion
+    }
+}
