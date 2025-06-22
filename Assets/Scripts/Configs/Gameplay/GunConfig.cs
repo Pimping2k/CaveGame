27f31ccb831444661bdff67687dce89178b69239
Pimping . <auagfuiawg@mail.ru>
2025-06-22 12:00:34 +0300

    Added gun config Reorder configs to folders

---
+++ Assets/Scripts/Configs/Gameplay/GunConfig.cs
@@ -0,0 +1,20 @@
+﻿using CustomEditorAttributes;
+using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Configs.Gameplay
+{
+    [CreateAssetMenu(fileName = "GunConfig_00", menuName = Data.Paths.GAMEPLAY + "Gun Config")]
+    public class GunConfig : ScriptableObject
+    {
+        [PimpingHeader("Damage Settings", HeaderColor.Indigo)]
+        [SerializeField] private float _damage;
+        [SerializeField] private float _weakSpotDamageMultiplier;
+        [SerializeField] [MinMaxRangeSlider(0f,5f)] private Vector2 _heatDamageMultiplier;
+
+        [PimpingHeader("Heat Settings", HeaderColor.Indigo)] 
+        [SerializeField] private float _heatCooldown;
+        [SerializeField] private float _heatStartTime;
+    }
+}
